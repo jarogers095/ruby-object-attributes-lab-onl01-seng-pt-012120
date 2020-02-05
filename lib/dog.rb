4e5e2efd777d3ae1e4b3527ed7d9f1aa49a8ed79
James Rogers <jarogers095@gmail.com>
2020-02-05 06:05:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,12 @@ class Dog
     @this_dogs_name
   end
   
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  def breed=(dogs_breed)
+    @this_dogs_breed = dogs_breed
   end
   
-  def name
-    @this_dogs_name
+  def breed
+    @this_dogs_breed
   end
   
 end
